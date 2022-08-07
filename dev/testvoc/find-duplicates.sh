c870eaad636d7f9f02ce0bee3514049e1a20377f
Marc Riera Irigoyen <marc.riera.irigoyen@gmail.com>
2022-08-07 22:19:37 +0200

    find-duplicates.sh

--- dev/testvoc/find-duplicates.sh
+++ dev/testvoc/find-duplicates.sh
@@ -1,27 +1,103 @@
 #!/bin/bash
 
-# This script looks for duplicate entries,
-# which may cause double generation errors.
-# Path to monolingual dix/metadix is autodetected.
-# Metadix requires compilation before running.
+set -e -u
 
-SRCDIR=$(cd ../.. && pwd)
+BILINGUAL=false
+
+show_help () {
+    cat >&2 <<EOF
+USAGE: $0 [ -b ] [ dix ]
+
+ -b, --bilingual: find bilingual duplicates (check dix on both sides)
+ -h, --help:      show this help
+
+Look for duplicate entries, which may cause double generation errors.
+If no path is specified, the monodix/bidix will be autodetected.
+Metadix requires compilation before running.
+
+EOF
+    exit 1
+}
+
+if [ $# -gt 0 ]; then
+    while :; do
+        case $1 in
+            -b|--bilingual)
+                BILINGUAL=true
+                ;;
+            -h|-\?|--help)
+                show_help
+                ;;
+            --)
+                shift
+                break
+                ;;
+            *)
+                break
+        esac
+        shift
+    done
+fi
+
+if [[ $# -eq 0 ]]; then
+    DIX=guess
+else
+    DIX=$1
+fi
+
+SRCDIR=$(pwd)
+for i in {1..3}; do
+    if [[ -e $SRCDIR/modes.xml ]]; then
+        break
+    fi
+    cd '..'
+    SRCDIR=$(pwd)
+done
+
 LANG1=$(grep -m1 "^LANG1 =" $SRCDIR/Makefile | sed "s/^.*= //")
+LANG2=$(grep -m1 "^LANG2 =" $SRCDIR/Makefile | sed "s/^.*= //")
+if [[ "$LANG2" ]]; then
+    BILINGUAL=true
+fi
 
-MONODIX="$SRCDIR/apertium-$LANG1.$LANG1.dix"
-if ! [[ -e $MONODIX ]]; then
-    MONODIX="$SRCDIR/.deps/apertium-$LANG1.$LANG1.dix"
-    if ! [[ -e $MONODIX ]]; then
-        echo "Monolingual dictionary ($MONODIX) not found."
-        exit 1
+if [[ $DIX = "guess" ]]; then
+    if $BILINGUAL; then
+        DIX="$SRCDIR/apertium-$LANG1-$LANG2.$LANG1-$LANG2.dix"
+        if ! [[ -e $DIX ]]; then
+            DIX="$SRCDIR/.deps/apertium-$LANG1-$LANG2.$LANG1-$LANG2.dix"
+            if ! [[ -e $DIX ]]; then
+                echo "Bilingual dictionary ($DIX) not found."
+                exit 1
+            fi
+        fi
+    else
+        DIX="$SRCDIR/apertium-$LANG1.$LANG1.dix"
+        if ! [[ -e $DIX ]]; then
+            DIX="$SRCDIR/.deps/apertium-$LANG1.$LANG1.dix"
+            if ! [[ -e $DIX ]]; then
+                echo "Monolingual dictionary ($DIX) not found."
+                exit 1
+            fi
+        fi
     fi
 fi
 
-lt-expand $MONODIX | grep -v 'REGEX' | grep -v ':>:' |
-rev | sed -E 's/:<:|:/\t/'| rev |
-sort -t$'\t' -bk 2 |
-sed -E 's/(<[^ ]*>)([^\t]*\t.*)/\1\2\t\1/' |
-uniq -f 1 -D |
-awk -F $'\t' '{print $2 "   --------->   " $1}' > duplicates.$LANG1.txt
+find_duplicates () {
+    if $BILINGUAL; then
+        awk -F'\t' 'a[$0]++ {print $1 " " $2 " " $3}'
+    else
+        # Check only generation, taking preferences into account
+        grep "<--" \
+        | sed -E 's/(<[^ ]*>)(([^\t<]*)(<[^ ]*>)*)(\t.*)/\1\2\5\t\1\4/' \
+        | awk -F'\t' 'a[$2$3$4]++ {print $1 " " $2 " " $3}'
+    fi
+}
+
+lt-expand $DIX \
+| sed '/:[<>]:/b;s/\\:/§§§/g;s/:/:-:/g;s/§§§/\\:/g' \
+| awk -F':[<>\\-]:' '
+          $0 ~ /:-:|:>:/ {print $1 "\t-->\t" $2}
+          $0 ~ /:-:|:<:/ {print $1 "\t<--\t" $2}' \
+| find_duplicates
 
 exit 0
